acea70ede7f7d4f19f30386acc3c352564f70520
ahmedhossam1212 <pmedo1689@gmail.com>
2023-07-29 11:11:50 +0300

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:iti_flutter_intern/view/screens/login_screen.dart';
+import 'package:iti_flutter_intern/view/screens/task_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
+    return const MaterialApp(
+      home: TaskView(),
     );
   }
 }
